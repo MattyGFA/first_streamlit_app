c90a0a3977e24ecf3bb6f706ab1609c988125a1b
MattyGFA <110372050+MattyGFA@users.noreply.github.com>
2022-08-01 14:38:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothy')
+streamlit.text('Hard-Bolied Free-Range Egg')
