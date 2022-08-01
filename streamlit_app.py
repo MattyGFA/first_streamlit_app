d01f3516499963d7ca393b2ee8815ac17a399e1c
MattyGFA <110372050+MattyGFA@users.noreply.github.com>
2022-08-01 14:42:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit
 
-streamlit.title('New Healthy Diner')
+streamlit.title('Mats Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Sausage, Bacon, Egg (Done your way), Beans, Mushrooms, Fried Bread')
+streamlit.text('B.E.S.T')
+streamlit.text('Toast')
+
+streamlit.header('Healthy Options')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothy')
 streamlit.text('Hard-Bolied Free-Range Egg')
+
+
