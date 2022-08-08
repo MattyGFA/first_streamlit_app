83c0684b6ffeb826cb441331e4372b1401f6519c
MattyGFA <110372050+MattyGFA@users.noreply.github.com>
2022-08-08 15:51:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.errorimport URLError
 
 streamlit.title('Mats Diner')
-
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🥖🥓🥚🍄🍞 Sausage, Bacon, Eggs(Done your way), Beans, Mushrooms, Fried Bread')
@@ -18,31 +21,28 @@ streamlit.text('🥑🍞 Adovado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Display the table on the page.
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 # Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # Output it as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# Snowflake Connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
@@ -50,5 +50,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# Snowflake add fruit
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
 streamlit.write('Adding: ', add_my_fruit)
